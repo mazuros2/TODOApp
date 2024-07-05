cc557f3068924012702aac32390405f52b3474e6
Mazuros2 <s24440@pjwstk.edu.pl>
2024-07-05 13:45:39 +0200

    project innit

---
+++ TODOapp-code/src/main/java/com/example/demo/Note/Note.java
@@ -0,0 +1,36 @@
+package com.example.demo.Note;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String category;
+    @NotBlank
+    private String description;
+    @NotNull
+    private LocalDate date;
+
+
+
+
+}
---
+++ TODOapp-code/src/main/java/com/example/demo/User/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.User;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String userName;
+    @NotBlank
+    private String userEmail;
+    @NotBlank
+    private String password;
+
+
+
+}
